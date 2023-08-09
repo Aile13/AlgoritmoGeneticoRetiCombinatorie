239f4b87c40d48d68eb192444f962d090b8f4f68
Aile <600eli@gmail.com>
2023-08-09 14:52:42 +0200

    Modifico numero variabili max e quali alg eseguire. Poi ripeto 10 volte test.

--- src/it/unibs/eliapitozzi/Main.java
+++ src/it/unibs/eliapitozzi/Main.java
@@ -17,43 +17,48 @@ import org.logicng.transformations.simplification.AdvancedSimplifier;
 public class Main {
     public static void main(String[] args) throws ParserException {
 
-        for (int i = 2; i < 27; i++) {
-            System.out.println("\tFunzione con " + i + " variabili di input:");
+        for (int j = 0; j < 10; j++) {// ciclo per fare 10 test.
+            for (int i = 2; i < 10; i++) {  // ciclo sul numero di variabili.
+                System.out.println("\tFunzione con " + i + " variabili di input:");
 
-            TabellaDiVerita tabellaDiVerita = new TabellaDiVerita(i);
-            StopWatch watch = StopWatch.create();
+                TabellaDiVerita tabellaDiVerita = new TabellaDiVerita(i);
+                StopWatch watch = StopWatch.create();
 
-            watch.start();
-            String function1 = AlgoritmoGenetico.run(tabellaDiVerita);
-            watch.stop();
-            System.out.println("Algoritmo genetico: " + watch);
-            esaminaFunzione(function1, tabellaDiVerita);
-
-            if (i <= 5) {
-                watch.reset();
                 watch.start();
-                QuineMcCluskey quineMcCluskey = new QuineMcCluskey(tabellaDiVerita.getVariabili(), tabellaDiVerita.getPosizioni());
-                String function2 = quineMcCluskey.getFunction();
+                String function1 = AlgoritmoGenetico.run(tabellaDiVerita);
                 watch.stop();
-                System.out.println("Algoritmo Quine-McCluskey: " + watch);
-                function2 = QuineMcCluskeyParseFunction.parseFunction(function2);
-                esaminaFunzione(function2, tabellaDiVerita);
+                System.out.println("Algoritmo genetico: " + watch);
+                esaminaFunzione(function1, tabellaDiVerita);
+
+                if (i <= 5) {
+                    watch.reset();
+                    watch.start();
+                    QuineMcCluskey quineMcCluskey = new QuineMcCluskey(tabellaDiVerita.getVariabili(), tabellaDiVerita.getPosizioni());
+                    String function2 = quineMcCluskey.getFunction();
+                    watch.stop();
+                    System.out.println("Algoritmo Quine-McCluskey: " + watch);
+                    function2 = QuineMcCluskeyParseFunction.parseFunction(function2);
+                    esaminaFunzione(function2, tabellaDiVerita);
+                }
+
+
+                if (i <= 8) {
+                    AdvancedSimplifier advancedSimplifier = new AdvancedSimplifier();
+
+                    FormulaFactory f = new FormulaFactory();
+                    PropositionalParser p = new PropositionalParser(f);
+
+                    watch.reset();
+                    watch.start();
+                    Formula formulaTabellaDiVerita = p.parse(tabellaDiVerita.getSumOfProducts());
+                    Formula applied = advancedSimplifier.apply(formulaTabellaDiVerita, true);
+                    watch.stop();
+                    System.out.println("Advanced Simplifier by LogicNG: " + watch);
+                    String function3 = applied.toString();
+                    esaminaFunzione(function3, tabellaDiVerita);
+                }
             }
-
-
-            AdvancedSimplifier advancedSimplifier = new AdvancedSimplifier();
-
-            FormulaFactory f = new FormulaFactory();
-            PropositionalParser p = new PropositionalParser(f);
-
-            watch.reset();
-            watch.start();
-            Formula formulaTabellaDiVerita = p.parse(tabellaDiVerita.getSumOfProducts());
-            Formula applied = advancedSimplifier.apply(formulaTabellaDiVerita, true);
-            watch.stop();
-            System.out.println("Advanced Simplifier by LogicNG: " + watch);
-            String function3 = applied.toString();
-            esaminaFunzione(function3, tabellaDiVerita);
+            System.out.println("------------   Fine esecuzione di un test --------------");
         }
 
     }
